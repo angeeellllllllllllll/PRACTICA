22b9be1f69b6883d0f429244ce117bd4cd90950e
student <student@MOB-012>
2024-03-22 17:03:46 +0400

    Practica day 1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Windows;
+using WpfApp1.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Vhod_Click(object sender, RoutedEventArgs e)
+        {
+            var login = log.Text;
+            var password = pass.Text;
+            var context = new AppDbContext();
+            var user_exist = context.Users.FirstOrDefault(x => x.Login == login && x.Password == password);
+            if (user_exist != null)
+            {
+                MessageBox.Show("Неправильный  логин или пароль");
+                return;
+            }
+            MessageBox.Show("Вы успешно вошли в аккаунт");
+        }
+
+        private void ToReg_Click(object sender, RoutedEventArgs e)
+        {
+            Registration registration = new Registration();
+            registration.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Windows/Registration.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace WpfApp1.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void RegBut_Click(object sender, RoutedEventArgs e)
+        {
+            var login = log.Text;
+            var password = Pass.Text;
+            var email = mail.Text;
+            var context = new AppDbContext();
+            var user_exist = context.Users.FirstOrDefault(x => x.Login == login && x.Password == password);
+            if (user_exist is not null)
+            {
+                MessageBox.Show("Такой пользователь уже существует");
+                return;
+            }
+            var user = new User { Login = login, Password = password };
+            context.Users.Add(user);
+            context.SaveChanges();
+
+            MessageBox.Show("Добро пожаловать!");
+
+        }
+    }
+}
